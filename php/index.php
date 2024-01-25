9c0e8dc6288ea37b6b784bed478bca4692038f3d
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-01-25 15:35:32 +0100

    add: aggiunto lista tramite backend

--- php/index.php
+++ php/index.php
@@ -1,11 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    funziono
-</body>
-</html>
+<?php
+
+header(('content-type: application/json'));
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Origin: http://localhost:5173");
+$lista = [
+    [
+        'compito' => 'comprare il pane ',
+        'done' => false
+    ],
+    [
+        'compito' => 'comprare il cibo per cani ',
+        'done' => true
+    ],
+    [
+        'compito' => 'pulire la casa ',
+        'done' => true
+    ]
+];
+
+$json_lista = json_encode($lista);
+echo $json_lista;
--- src/components/lista.vue
+++ src/components/lista.vue
@@ -1,9 +1,34 @@
 <script>
+import axios from "axios";
 export default {
   new: "lista",
+  data() {
+    return {
+      array_lista: [],
+    };
+  },
+
+  mounted() {
+    const t = this;
+    axios.get("http://localhost/php-todo-list-json/php/").then((risposta) => {
+      t.array_lista = risposta.data;
+      console.log(risposta.data);
+    });
+  },
 };
 </script>
 
-<template>sono lista</template>
+<template>
+  <div class="container">
+    <div class="row">
+      <h1 class="text-center">compiti della lista {{ array_lista.length }}</h1>
+      <ul>
+        <li v-for="(compiti, i) in array_lista" :key="i">
+          {{ compiti.compito }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
 
 <style scoped lang="scss"></style>
