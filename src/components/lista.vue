4ee4df956b3149a921a209dd8e5fc02dee59013a
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-01-25 16:30:25 +0100

    add: aggiunto controllo errori

--- src/components/lista.vue
+++ src/components/lista.vue
@@ -20,10 +20,15 @@ export default {
   mounted() {
     const t = this;
     // chiamata ad api per il nostro server
-    axios.get("http://localhost/php-todo-list-json/php/").then((risposta) => {
-      t.array_lista = risposta.data;
-      // console.log(risposta.data);
-    });
+    axios
+      .get("http://localhost/php-todo-list-json/php/")
+      .then((risposta) => {
+        t.array_lista = risposta.data;
+        // console.log(risposta.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   },
 };
 </script>
