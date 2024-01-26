36cb91dac9e466f9903bea80112c09b614b54860
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-01-26 10:14:46 +0100

    add: aggiornando lista usando il props

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,56 @@
 <script>
 // impost del file lista
 import lista from "./components/lista.vue";
+import axios from "axios";
 
 export default {
   components: {
     // componente lista
     lista,
   },
+  data() {
+    return {
+      array_lista: [],
+      compitoDaAggiungere: "",
+    };
+  },
+  methods: {
+    // fnzione per aggiungere la tasks
+    aggiungielemento() {
+      this.array_lista.push({ compito: this.compitoDaAggiungere, done: true });
+      this.compitoDaAggiungere = "";
+    },
+  },
+  mounted() {
+    // chiamata ad api per il nostro server
+    axios
+      .get("http://localhost/php-todo-list-json/php/")
+      .then((risposta) => {
+        this.array_lista = risposta.data;
+        // console.log(risposta.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
 };
 </script>
 
 <template>
-  <lista />
+  <div class="container">
+    <div class="row">
+      <h1 class="text-center">compiti della lista {{ array_lista.length }}</h1>
+      <div>
+        <input type="text" v-model="compitoDaAggiungere" />
+      </div>
+      <button style="width: 200px" @click="aggiungielemento">aggiungi</button>
+      <ul>
+        <li v-for="(lista, i) in array_lista">
+          <lista :list="lista" />
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
--- front/src/components/lista.vue
+++ front/src/components/lista.vue
@@ -2,32 +2,8 @@
 import axios from "axios";
 export default {
   new: "lista",
-  data() {
-    return {
-      // arrai dove ci saranno tutti i compiti
-      array_lista: [],
-      compitoDaAggiungere: "",
-    };
-  },
-  methods: {
-    // fnzione per aggiungere la tasks
-    aggiungielemento() {
-      this.array_lista.push({ compito: this.compitoDaAggiungere, done: true });
-      this.compitoDaAggiungere = "";
-    },
-  },
-
-  mounted() {
-    // chiamata ad api per il nostro server
-    axios
-      .get("http://localhost/php-todo-list-json/php/")
-      .then((risposta) => {
-        this.array_lista = risposta.data;
-        // console.log(risposta.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  props: {
+    list: Object,
   },
 };
 </script>
@@ -35,27 +11,11 @@ export default {
 <template>
   <div class="container">
     <div class="row">
-      <h1 class="text-center">compiti della lista {{ array_lista.length }}</h1>
-      <p>
-        in rosso i compiti che non hai completato <br />in verde i compiti che
-        hai completato
-      </p>
-      <!-- aggiunta tasks -->
-      <div>
-        <input type="text" v-model="compitoDaAggiungere" />
+      <div v-if="list.done">{{ list.compito }}</div>
+
+      <div v-else>
+        <del>{{ list.compito }}</del>
       </div>
-      <button style="width: 200px" @click="aggiungielemento">aggiungi</button>
-      <ul>
-        <!-- ciclo per comparsa componenti -->
-        <li v-for="(compiti, i) in array_lista" :key="i">
-          <div v-if="compiti.done">
-            {{ compiti.compito }}
-          </div>
-          <div v-else>
-            <del>{{ compiti.compito }}</del>
-          </div>
-        </li>
-      </ul>
     </div>
   </div>
 </template>
